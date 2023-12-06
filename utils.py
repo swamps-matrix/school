f6eccf3a4644661da560090d64c149a3beca637c
Sebactian Monroe <152859515+swamps-matrix@users.noreply.github.com>
2023-12-06 10:45:52 +0600

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+# utils.py
+def display_menu():
+    print("1. Add Student")
+    print("2. Add Teacher")
+    print("3. Add Course")
+    print("4. Display School Info")
+    print("5. Exit")
+
+def get_user_choice():
+    return input("Enter your choice: ")
